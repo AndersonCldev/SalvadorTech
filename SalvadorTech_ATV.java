2f467efe2fc655caf0672e3aa67580442bbcba0a
Anderson Cleiton <135235923+AndersonCldev@users.noreply.github.com>
2024-03-01 10:48:49 -0300

    Update SalvadorTech_ATV.java
    Adição de uma Variável "soma" e Exibição da Soma:
    
    Adicionamos uma variável soma para acumular a soma dos números ímpares.
    Após o loop que exibe os números ímpares, adicionamos a exibição da soma (System.out.println("Soma dos números ímpares: " + soma);).
    Isso ocorre apenas quando o número inserido pelo usuário é ímpar (numeros % 2 != 0).
    Ajuste na Exibição de Números Pares e Quadrados:
    
    Ajustamos a mensagem de exibição para os números pares e seus quadrados, apenas para melhor clareza na saída.

--- SalvadorTech_ATV.java
+++ SalvadorTech_ATV.java
@@ -12,20 +12,30 @@ public class SalvadorTech_ATV {
         System.out.println("A partir daqui, escreva 2 números...");
 
         for (int i = 0; i < 2; i++) {
-            System.out.println("Escreva um número:");
+        	System.out.println(" ");
+            System.out.print("Escreva um número:");
+            System.out.println(" ");
             numeros = ler.nextInt();
 
             if (numeros % 2 != 0) {
                 System.out.println("Números ímpares até " + numeros + ":");
+                int soma = 0;
 
                 for (int k = 1; k <= numeros; k += 2) {
                     System.out.println(k);
+                    soma += k;
                 }
-            } else {
-                System.out.println("Números pares ao quadrado até " + numeros + ":");
 
+                System.out.println("Soma dos números ímpares: " + soma);
+            } else {
+            	
+                System.out.println("Números pares ao quadrado até " + numeros + ":");
+                System.out.println(" ");
+                
                 for (int j = 0; j <= numeros; j += 2) {
-                    System.out.println(j * j);
+                    System.out.println("Numeros pares: " + j);
+                    
+                    System.out.println("Quadrado dos pares: " + j * j);
                 }
             }
         }
