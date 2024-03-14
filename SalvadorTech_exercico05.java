df21f6ad000e715a0130a62b34dd1b3056259739
Anderson Cleiton <135235923+AndersonCldev@users.noreply.github.com>
2024-03-14 12:23:33 -0300

    Tabuada escolhida
    Nesta atividade foi pedido para criarmos um código recebe do usuário qual número da tabela da atabuada ele quer, após escolher o código receber o número escolhido e através de um loop for faca tabuada do número escolhido.

---
+++ SalvadorTech_exercico05.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+//Este código recebe do úsuario um número que ele quer a tabuada e o programa entra em um loop for para mostrar estas operações 
+public class SalvadorTech_exercico05 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int tabuadaEscolhida, resultado;
+			
+		System.out.println("Escreva até onde o programa deve contar: ");
+		tabuadaEscolhida = ler.nextInt();
+		
+		System.out.println("Número escolhido: " + tabuadaEscolhida);
+		System.out.println("______________________________________");
+		for(int i = tabuadaEscolhida; i <= tabuadaEscolhida ; i ++) {
+			
+			for(int j = 0; j<= 10; j++) {
+				
+				resultado = i * j;
+				System.out.println(i +" x " + j +  " = "+ resultado);
+			}
+			
+		}
+
+		System.out.println("Fim");
+		
+
+	}
+
+}
